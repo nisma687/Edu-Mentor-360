e09355b7e164d354d1939160d8aa21209af57da9
Nisma <20-41982-1@student.aiub.edu>
2023-11-30 06:02:37 +0600

    done with extra 2 section of home page

--- src/pages/Home/CarrerGuidelines/CarrerGuidelines.jsx
+++ src/pages/Home/CarrerGuidelines/CarrerGuidelines.jsx
@@ -7,7 +7,104 @@ const CarrerGuidelines = () => {
             <div>
                 <Tittle heading={"Confused??"}
                     subHeading={"Read Some articles for carrer guidelines"} />
-                    <TfiFaceSad />
+                    <div className="flex justify-center">
+                    <TfiFaceSad  className="items-center" />
+                    </div>
+                    <div className="grid grid-cols-2 gap-5 m-5">
+                        <div className="p-4 rounded bg-gradient-to-r from-purple-200 hover:from-violet-300 hover:to-blue-200">
+                            <p className="text-xl text-black font-semibold italic">
+                                Most of the students always find it difficult to choose the right career path for themselves. They are always confused about what to do after 12th. They are always in a dilemma about which career option to choose. and what is the best career option for them. They are always confused about which career option to choose. and what is the best career option for them. If you are also one of them, then you are at the right place. Here we will discuss the best career options after 12th. We will also discuss the best career options after 10th. We will also discuss the best career options after graduation. We will also discuss the best career options after post-graduation. We will also discuss the best career options after 10th. We will also discuss the best career options after graduation. We will also discuss the best career options after post-graduation.For More <a href="https://www.elderwisdomcircle.org/ask-for-advice/?gad_source=1&gclid=Cj0KCQiA35urBhDCARIsAOU7QwkzrWbg-g8NJ-Fs0-j8pvZ8TPgc9X8WJxettQG185e9JgJRC_-SA4IaAppuEALw_wcB">
+                                    <span className="text-blue-700 font-bold">Click Here</span>
+                                </a>
+                            </p>
+                        </div>
+                       
+                        <div className="p-4 italic text-center bg-gradient-to-r from-purple-200 hover:from-violet-300 hover:to-blue-200">
+                                <p className="font-semibold text-xl ">
+                                Some Resources You can Look to:
+                                <br />
+                                </p>
+                                <ul className="mt-3">
+                                    <li>
+                                        <a href="https://www.elderwisdomcircle.org/ask-for-advice/?gad_source=1&gclid=Cj0KCQiA35urBhDCARIsAOU7QwkzrWbg-g8NJ-Fs0-j8pvZ8TPgc9X8WJxettQG185e9JgJRC_-SA4IaAppuEALw_wcB">
+                                    <span className="text-blue-700 font-bold">Elder Wisdom Circle</span>
+                                </a>
+                                </li>
+                                <li><a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">Career Explorer</span>
+                                </a></li>
+                                <li><a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">Today or Never</span>
+                                </a></li>
+                                <li><a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">Failure means Try again</span>
+                                </a></li>
+                                </ul>
+                                <p className="text-xl text-black font-semibold"> More Resources : </p>
+                                    <ul className="mt-3">
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                        Your Career Guide
+                                    </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                        Career Advice
+                                    </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                        Finish what you started
+                                    </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                      Never Give Up
+                                        </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                       Best Career Options
+                                        </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                       Abroad Opportunities
+                                        </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                        
+                                       Life is a Journey
+                                        </span>
+                                </a>
+                                   </li>
+                                   <li>
+                                   <a href="https://www.careerexplorer.com/careers/">
+                                    <span className="text-blue-700 font-bold">
+                                        Carrer is a Marathon
+                                        </span>
+                                </a>
+                                   </li>
+                                    
+                                </ul>
+
+                                
+                        </div>
+                    </div>
 
             </div>
         </div>
